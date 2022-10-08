f10dd89f4e068cfc4119d59ea63029f5e7d972c7
ozalperen <alperenoz93@gmail.com>
2022-10-08 17:01:11 +0300

    show cached flights

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -1,19 +1,14 @@
-import express from 'express';
-import {getFlightHandler } from '../controllers/flight.controller';
-import { deserializeUser } from '../middleware/deserializeUser';
-import { requireUser } from '../middleware/requireUser';
-import { validate } from '../middleware/validate';
-import { getFlightSchema } from '../schemas/flight.schema';
-
+import express from "express";
+import { getFlightHandler } from "../controllers/flight.controller";
+import { deserializeUser } from "../middleware/deserializeUser";
+import { requireUser } from "../middleware/requireUser";
+import { validate } from "../middleware/validate";
+import { getFlightSchema } from "../schemas/flight.schema";
 
 const router = express.Router();
 
 router.use(deserializeUser, requireUser);
-router
-  .route('/')
-  .post(
-    getFlightHandler,
-    validate(getFlightSchema));
+router.route("/").post(getFlightHandler, validate(getFlightSchema));
 
 
 export default router;
